6ce95c156204e98ccb833c56fe1739e4a6f5474e
Denny Kunker <wkunker@gmail.com>
2016-12-09 08:41:38 -0500

    Scripts now make it all the way through (assuming the database is already configured). NRPE now gets installed on the Icinga host as well.

--- deploy/centos7/install.sh
+++ deploy/centos7/install.sh
@@ -6,12 +6,17 @@ sudo yum -y update
 sudo yum -y install epel-release
 sudo yum -y install mysql mariadb-server MySQL-python httpd icinga2
 sudo rpm --import http://packages.icinga.org/icinga.key
-sudo rpm -i https://packages.icinga.org/epel/7/release/noarch/icinga-rpm-release-7-1.el7.centos.noarch.rpm
+yum list installed icinga-rpm-release
+if [ $(echo "$?") -ne "0" ]; then
+	sudo rpm -i https://packages.icinga.org/epel/7/release/noarch/icinga-rpm-release-7-1.el7.centos.noarch.rpm
+fi
 
 MYCNF_PATH="${HOME}/.my.cnf"
 if [ ! -f ${MYCNF_PATH} ]; then
 	echo "[client]" > ${MYCNF_PATH}
 	echo "user=root" >> ${MYCNF_PATH}
+	echo "password=TZHiNB9Rh5MV" >> ${MYCNF_PATH}
+	sudo cp ${MYCNF_PATH} /root/
 fi
 
 sudo ansible-playbook playbook.yml
