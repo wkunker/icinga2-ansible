0c9e6822fef61f608075de50cee81764fad7b94a
Denny Kunker <wkunker@gmail.com>
2016-12-08 23:36:01 -0500

    Added install scripts for centos7.

---
+++ deploy/centos7/icinga-install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+sudo yum -y update
+sudo yum -y install epel-release
+sudo yum -y install mysql mariadb-server MySQL-python httpd icinga2
+sudo rpm --import http://packages.icinga.org/icinga.key
+sudo rpm -i https://packages.icinga.org/epel/7/release/noarch/icinga-rpm-release-7-1.el7.centos.noarch.rpm
+
+MYCNF_PATH="${HOME}/.my.cnf"
+if [ ! -f ${MYCNF_PATH} ]; then
+	echo "[client]" > ${MYCNF_PATH}
+	echo "user=root" >> ${MYCNF_PATH}
+fi
+
+sudo ansible-playbook icinga.yml
+
